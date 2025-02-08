9274461f6f3a4cdd0e7bde7e34d46cd5f93d857e
giuRa13 <giulioravazzolobass@gmail.com>
2025-02-08 23:38:36 +0100

    Add project files.

---
+++ StockMarket.Domain/Models/Stock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket.Domain.Models
+{
+    public class Stock
+    {
+        public string Symbol { get; set; }
+        public double PricePerShare {  get; set; }
+
+    }
+}
---
+++ StockMarket.EntityFramework/StockMarketDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockMarket.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockMarket.EntityFramework
+{
+    public class StockMarketDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set;}
+
+        public DbSet<Account> Accounts { get; set; }
+
+        public DbSet<AssetTransaction> AssetTransactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AssetTransaction>().OwnsOne(a => a.Stock); //Stock embedded in AssetTransactions table
+
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=GIULI13\\SQLEXPRESS;Initial Catalog=StockMarketDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
